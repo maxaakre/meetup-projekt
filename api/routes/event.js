7d70e08fba791fa44fa2baec9fae310109fa9beb
maxaakre <maxaakre@gmail.com>
2020-09-30 15:34:50 +0200

    added docker 2

--- api/routes/event.js
+++ api/routes/event.js
@@ -8,15 +8,7 @@ router.get("/products", async (req, res) => {
   const products = await Newevent.getAll();
   res.status(201).json(products);
 });
-// //GET ONE PRODUCT
-// router.get("/:id", async (req, res) => {
-//   const products = await Newevent.get(req.params.id);
-//   if (products) {
-//     res.status(201).json(products);
-//   } else {
-//     res.status(401).json({ message: "Event not found!" });
-//   }
-// });
+
 //CREATE NEW PRODUCT
 router.post("/event", async (req, res) => {
   if (req.user.role === "customer") {
